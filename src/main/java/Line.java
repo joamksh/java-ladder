14896c6ab0e154ca2073addef6668b8b09296a2e
joamksh <joamksh@naver.com>
2024-07-06 23:22:44 +0900

    feat : LineClass구현 이전위치에 가로선 있으면 false 입력

---
+++ src/main/java/Line.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Line {
+    private List<Boolean> points;
+
+    public Line(int personCount) {
+        this.points = generatePoints(personCount);
+    }
+
+    private List<Boolean> generatePoints(int personCount) {
+        List<Boolean> points = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < personCount - 1; i++) {
+            if (i > 0 && points.get(i - 1)) {
+                points.add(false); // 이전 위치에 가로선이 있으면 현재 위치에는 가로선을 두지 않음
+            } else {
+                points.add(random.nextBoolean());
+            }
+        }
+
+        return points;
+    }
+
+    public boolean hasPoint(int index) {
+        return points.get(index);
+    }
+
+    public int size() {
+        return points.size();
+    }
+}
