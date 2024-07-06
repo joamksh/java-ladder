dbfaa861433cd58925eca46761bc8d7d2cffbe34
joamksh <joamksh@naver.com>
2024-07-06 22:19:28 +0900

    feat : PersonClass 이름, 위치 정보 기본 도메인 클래스 구현

---
+++ src/main/java/Person.java
@@ -0,0 +1,25 @@
+public class Person {
+    String name;
+    int position;
+
+    public Person(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+}
