dc2d1cac9c48f911e1c936245b6076949749d369
joamksh <joamksh@naver.com>
2024-07-07 09:25:39 +0900

    ref : input/output view -main 2단계 내용 추가

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -11,8 +11,19 @@ public class InputView {
         return Arrays.asList(input.split(","));
     }
 
+    public static List<String> inputResults() {
+        System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        String input = scanner.nextLine();
+        return Arrays.asList(input.split(","));
+    }
+
     public static int inputLadderHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static String inputQuery() {
+        System.out.println("결과를 보고 싶은 사람은?");
+        return scanner.nextLine();
     }
 }
--- src/main/java/OutputView.java
+++ src/main/java/OutputView.java
@@ -1,11 +1,12 @@
 import java.util.List;
 
 public class OutputView {
-    public static void printLadder(Members members, Ladder ladder) {
+    public static void printLadder(Members members, Ladder ladder, Result result) {
         printMembers(members.getMembers());
         for (Line line : ladder.getLines()) {
             printLine(line);
         }
+        printResults(result.getResults());
     }
 
     private static void printMembers(List<Member> members) {
@@ -24,4 +25,24 @@ public class OutputView {
         sb.append("|");
         System.out.println(sb.toString());
     }
+
+    private static void printResults(List<String> results) {
+        for (String result : results) {
+            System.out.print(result + "   ");
+        }
+        System.out.println();
+    }
+
+    public static void printResult(String result) {
+        System.out.println("실행 결과");
+        System.out.println(result);
+    }
+
+    public static void printAllResults(List<String> allResults) {
+        System.out.println("실행 결과");
+        for (String result : allResults) {
+            System.out.println(result);
+        }
+    }
+
 }
